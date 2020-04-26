d3f079db757a3edfc53661018c04f23a74eb982a
04prashant <prashant.puri@nagarro.com>
2020-04-26 18:16:26 +0530

    statedata-display

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,7 +9,7 @@ import { DetailsOfStateService } from '../services/details-of-state.service';
 export class DashboardComponent implements OnInit {
 
   data: any[];
-  sampledata: any[];
+  sampledata: any;
   constructor(private detailsOfStateservice: DetailsOfStateService) {
   }
 
@@ -20,20 +20,9 @@ export class DashboardComponent implements OnInit {
   getStatesData() {
     this.detailsOfStateservice.getStatesData()
     .subscribe(response => {
-      console.log(response);
-
-      console.log(Object.keys(response),'keys');
-     // this.data = Object.keys(response);
-       console.log(Object.entries(response),'entries');
-        this.data = Object.entries(response);
-        console.log(Object.values(response),'values');
-      //  this.data = Object.values(response);
-      // // console.log(this.data);
-      // //  this.data = JSON.stringify(response);
-      // this.sampledata = Object.values(response);
-      // for (let value of Object.values(this.sampledata)) {
-      //   console.log(value.districtData);
-      // }
+      this.data = Object.values(response);
+      console.log(this.data[1]);
+      this.sampledata = this.data[1];
     });
   }
 
